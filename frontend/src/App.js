cd9d22e3d131a1860129502bb91ebb7377d145a8
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-10 10:13:30 -0300

    Update App.js
    error en visualización

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Home from './pages';
-import cover from '../../frontend/src/images/cover.png';
 
 function App() {
   return (
