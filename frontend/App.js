3cfa46c737d9653e3b0c6026443ab3fd18eb5a84
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-10 10:38:54 -0300

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages/index';
+
+function App() {
+  return (
+    <Router>
+      <Home />
+    </Router>
+  );
+}
+
+export default App;
