dc6be966bd7375833c0ce6ce3ed8220141ced3fc
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-10 10:37:10 -0300

    Add files via upload

--- frontend/src/components/HeroSection/HeroElements.js
+++ frontend/src/components/HeroSection/HeroElements.js
@@ -19,7 +19,9 @@ export const HeroContainer = styled.div`
         left:0;
         right: 0;
         bottom: 0;
-        background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%),rgba(0,0,0,0.6)100%)
+        background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%),rgba(0,0,0,0.6)100%),
+        linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%)
+        z-index:1;
     }
 
     `
