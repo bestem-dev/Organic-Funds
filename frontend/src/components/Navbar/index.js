afc50daca4eb9fded0aee39b1352e4c49f6e5f49
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-25 00:26:53 -0300

    integracion de metamask

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,9 +2,57 @@ import React from "react";
 import { Nav, NavbarContainer, NavLogo, MobileIcon,NavBtnLink, NavBtn} from './NavbarElements';
 import {FaBars} from 'react-icons/fa';
 import imagenes2 from "../Navbar/imagenes2";
+import {useState} from "react";
 import nav from "./nav.css";
 
 const Navbar = ({toggle }) =>{
+    const Navbar = ({toggle }) =>{
+
+    
+  const [isConnected, setIsConnected]= useState(false)
+  const [walletAddress, setWallet] = useState('')
+
+  const pressedConnectWallet = async () => {
+
+    if(isConnected) return alert(
+        "cuenta conectada: "+walletAddress
+    )
+
+    const walletResponse = await connectWallet()
+    setIsConnected(walletResponse.connectedStatus)
+    setWallet(walletResponse.address)
+
+  }
+
+  const connectWallet = async () => {
+    if(window.ethereum){
+      try{
+        const address = await window.ethereum.request({
+          method: 'eth_requestAccounts'
+        })
+        const object = {
+          connectedStatus: true,
+          status: 'La conexion ha sido un exito',
+          address
+        }
+
+        return{
+          connectedStatus: false,
+          status: "error al conectar wallet"
+        }
+
+      } catch(err){
+
+      }
+
+    }else{
+      return{
+        connectedStatus: false,
+        status: "Metamask is not instaled in browser"
+      }
+    }
+  }
+
     return(
         <>
         <Nav id="nav">
