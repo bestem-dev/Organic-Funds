27736bba63ec8233a1a1e47dfdcc4af28ad84ee7
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-24 22:39:05 -0300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import Home from './pages/index';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Pages/index';
+import Contenido from './Pages/contenido';
+import Voting from './Pages/Voting';
+
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path='/'element={<Home />} />
+        <Route path='/contenido' element={<Contenido />} /> 
+        {/* <Route path='/metamask' element={<Metamask />} /> */}
+        <Route path='/Voting' element={<Voting />} />
+      </Routes>
     </Router>
   );
 }
