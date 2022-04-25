9af4d5e49edc7dfc4567e785ea1c37dc42712132
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-04-24 22:45:38 -0300

    Add files via upload

--- frontend/src/components/buttonElements.js
+++ frontend/src/components/buttonElements.js
@@ -3,11 +3,10 @@ import { Link } from "react-scroll";
 
 export const Button = styled(Link)`
     border-radius: 50px;
-    background: ${({primary}) => (primary ? '#16DF7E' : '#1FFF89')
+    
 };
 white-space: nowrap;
 padding: ${({big}) => (big ? '24px 48px' : '22px 30px')};
-color: ${({dark}) => (dark ? '#272F3C': '#ffffff')};
 font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
 outline:none;
 border:none;
@@ -17,14 +16,16 @@ justify-content: center;
 align-items: center;
 transition: all 0.2s ease-in-out;
 z-index:0;
-margin-top: 337px;
-margin-right:950px;
+margin-top: -245px;
+margin-right:280px;
+border: white 1px solid;
+color: white;
 
 &:hover{
     transition: all 0.2s ease-in-out;
-    background: ${({primary}) => ( primary ? '#fff':'#16DF7E')
+   
 };
-    color: #010606;
+   
 }
 
 `
